84041b3c936242cdcbdd788071c3a84e75b39d47
danny <dannycmendiola@gmail.com>
2024-10-29 21:24:38 -0700

    Automated commit message

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,21 +2,58 @@ import Dexie, { type EntityTable } from 'dexie';
 
 interface IMoment {
 	momentId: number;
-	title: string;
-	body: string;
+	name: string;
+	attr: string;
+	order: number;
+	locations: number[];
+	characters: number[];
+	themes: number[];
+}
+
+interface ILocation {
+	locationId: number;
+	name: string;
+	attr: string;
+}
+
+interface ICharacter {
+	characterId: number;
+	name: string;
+	attr: string;
+}
+
+interface ITheme {
+	themeId: number;
+	name: string;
 	attr: string;
 }
 
 const db = new Dexie('sidekick') as Dexie & {
 	moments: EntityTable<IMoment, 'momentId'>;
+	locations: EntityTable<ILocation, 'locationId'>;
+	characters: EntityTable<ICharacter, 'characterId'>;
+	themes: EntityTable<ITheme, 'themeId'>;
 };
 
-// db.version(1).stores
 db.version(1).stores({
-	// only indexed columns are specified here
-	moments: '++momentId, title'
+	moments: '++momentId, name, order, *locations, *characters, *themes',
+	locations: '++locationId, name',
+	characters: '++characterId, name',
+	themes: '++themeId, name'
 });
 
-class Moment {}
+class Moment {
+	momentId!: number;
+	name!: string;
+	attr?: string;
+	order!: number;
+	locations?: number[];
+	characters?: number[];
+	themes?: number[];
+
+	// TODO
+}
+
+db.moments.mapToClass(Moment);
 
 export { db };
